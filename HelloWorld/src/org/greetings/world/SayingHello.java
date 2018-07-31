303a753f398998e368a9aef5746dd33b3e3c0872
Robert Kirk <robert_ki@photoninfotech.net>
2018-07-31 09:26:12 -0500

    Testing Stuff up

---
+++ HelloWorld/src/org/greetings/world/SayingHello.java
@@ -0,0 +1,10 @@
+package org.greetings.world;
+
+public class SayingHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, I hope everyong will be able to see this message!");
+	}
+
+}
